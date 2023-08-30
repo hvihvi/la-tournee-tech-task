18f6286a8d328c9d835810e0ffbc0a89e615ede4
Hugo VINET <hvi@hugos-macbook-pro.home>
2023-08-30 22:05:01 +0200

    fix: increase contrast on caption text (removes lighthouse warining)

--- design-system/typography/Caption.tsx
+++ design-system/typography/Caption.tsx
@@ -9,7 +9,7 @@ export const Caption = (props: Props) => (
     className={lato.className}
     fontSize="14px"
     fontWeight="400"
-    color="rgba(0,0,0,0.5)"
+    color="rgba(0,0,0,0.6)"
     {...props}
   />
 );
